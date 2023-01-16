6464b1a92aab4581b456c3723980f8244bfb3533
Myrza-Alymbekov <myrza.bakytbekovich@gmail.com>
2023-01-17 01:17:49 +0600

    Second Commit

--- ecommerce_website/settings.py
+++ ecommerce_website/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'account',
     'shop',
 ]
@@ -127,3 +128,9 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
